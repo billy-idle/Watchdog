26e73bb179b6728eeff592557a4ea3248d67b5aa
Billy Idle <guillesupremacy@duck.com>
2024-03-13 21:49:27 -0500

    Initial commit

---
+++ src/test/java/com/github/watchdog/WatchdogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.watchdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchdogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
